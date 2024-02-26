4060fbd9c34f02f101357921b4475fcc2af7a35b
qa-alexey-reva <157701736+qa-alexey-reva@users.noreply.github.com>
2024-02-26 19:10:31 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-26",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
